8b4fb92b8e687c83a40b1287d4a2d7bbff6dbfc9
Ankit <ankit.anshu145@gmail.com>
2023-06-02 11:00:32 +0530

    SignUp logic with password encryption done

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+const { SALT } = require("../config/serverConfig");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -34,5 +36,9 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "User",
     }
   );
+  User.beforeCreate(async (user) => {
+    const encyrptedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = encyrptedPassword;
+  });
   return User;
 };
